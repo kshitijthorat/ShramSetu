a53dbdc0ec94f36215537e26ba55fb8423c8582a
kshitijthorat <kshitijthorat12@gmail.com>
2025-11-12 12:38:07 +0530

    admin pannel navbar added

--- src/pages/Admin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin.jsx
@@ -3,11 +3,13 @@ import AdminNavbar from '../../components/AdminNavbar'
 
 const adminLogin = () => {
   return (
+    <div>
     <div className='flex justify-center items-center flex-row '>
         <AdminNavbar/>
         <div className='w-full h-full border-amber-950 border-2'>
             heee
         </div>
+    </div>
     </div>
   )
 }
